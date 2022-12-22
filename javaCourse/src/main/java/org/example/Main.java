b47c65cd541e6ef16cfeff60ec60817d44ef8f52
magdy <magdyshenoda2021@gmail.com>
2022-12-22 14:52:19 +0300

    add

--- javaCourse/src/main/java/org/example/Main.java
+++ javaCourse/src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+
+        FileOutputStream fileOutputStream =new FileOutputStream("file.txt");
+        fileOutputStream.write("my name is marko".getBytes());
+
+        FileInputStream fileInputStream = new FileInputStream("file.txt");
+        int data;
+
+        while ((data=fileInputStream.read())!=-1){
+            System.out.println(data);
+        }
+
+
+
     }
 }
